42e2a76a8d06c31efe82af50c1179f22dffa6f5d
Mokhtar-dev <generalmk122@gmail.com>
2026-03-01 15:16:11 -0800

    used the fgets function instead of scanf to read the item name, which allows for spaces in the item name. I also added a check to ensure that the item name is not empty before adding it to the cart. Additionally, I updated the total price calculation to include the new item and quantity. Finally, I added a message to confirm that the item has been added to the cart successfully.

--- Programs_Made_With_C/Shopping_cart_program/Shopping_cart_program.c
+++ Programs_Made_With_C/Shopping_cart_program/Shopping_cart_program.c
@@ -5,15 +5,18 @@ int main()
     char item[50] = "";
     float price = 0.0;
     int quantity = 0;
-    char currency = '$';  // NOTE: when using char to store a singel character, you must use single quotes (' ') instead of double quotes (" ")
+    char currency = '$'; // NOTE: when using char to store a singel character, you must use single quotes (' ') instead of double quotes (" ")
     float total_price = price * quantity;
 
     printf("what item do you want to add to the shopping cart ?\n");
-    scanf("%s", item);
+    fgets(item, sizeof(item), stdin); // the fgets function is used to read a string and white space characters (like space or newline) are included in the input, so it can read the full name of the item
+    
     printf("what is the price for each ?\n");
     scanf("%f", &price);
+   
     printf("what is the quantity of the item you want to add to the shopping cart?\n");
     scanf("%d", &quantity);
+   
     total_price = price * quantity;
     printf("your total price is: %c%.2f\n", currency, total_price); // added the currency symbol before the total price and formatted it to 2 decimal places using %.2f
 
