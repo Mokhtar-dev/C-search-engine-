c8d69b471a447809acaabd234c0335e9294d3ff3
Mokhtar-dev <generalmk122@gmail.com>
2026-03-23 22:47:53 -0700

    Updated functions and it have parameter now.

--- 10_Functions.c
+++ 10_Functions.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 
 // Functions in C:
-    /*A function is a block of code that performs a specific task,
-  it is a reusable piece of code that can be called "invoked" multiple times in a program,
-   it helps to break down a large program into smaller and more manageable pieces,
-  it also helps to improve the readability and maintainability of the code*/
+/*A function is a block of code that performs a specific task,
+it is a reusable piece of code that can be called "invoked" multiple times in a program,
+it helps to break down a large program into smaller and more manageable pieces,
+it also helps to improve the readability and maintainability of the code*/
 
-void myFunction() {  // this is a function declaration, it defines a function named myFunction that takes no parameters and returns no value (void)
-    printf("This is a function in C\n");
+void myFunction(char name[], int age) {  // this is a function declaration and returns no value (VOID) "RETURN VALUE = 0"
+                     //  ↑
+// these where you put the parameters that holds the expected values of the function.
+    printf("YOU ARE GOOD AT PROGRAMMING %s! You are %d years old.\n", name, age);
 }
 
-int main() {
-    
+// NOTE: functions dont see what is inside of other functions so if you want to use a variable inside a function you need to pass it as a parameter or declare it as a    ADVANCED --> "global variable"
 
-    myFunction();  // this is a function call, it invokes the myFunction to execute the code inside it
-    myFunction();  // you can call the same function multiple times in a program
-    
+int main() {
+    char name[] = "Mokhtar";  // ---> this variable is not visible inside the myFunction because it is declared inside the main function and it is not passed as a parameter to the myFunction
+    int age = 18;             //---> same as the name variable.
+
+    myFunction(name, age);  // this is a function call, it invokes the myFunction to execute the code inside it
+    myFunction(name, age);  // you can call the same function multiple times in a program
+    myFunction(name, age);  // when invoking a function it expects arguments based on the prameter from earlier "MANDATORY"
+
+    //OUTPUT:
+    //YOU ARE GOOD AT PROGRAMMING Mokhtar! You are 18 years old.
+    //YOU ARE GOOD AT PROGRAMMING Mokhtar! You are 18 years old.
+    //YOU ARE GOOD AT PROGRAMMING Mokhtar! You are 18 years old.
+               
     return 0;
 }
--- Learnning_C.c
+++ Learnning_C.c
@@ -200,5 +200,40 @@ int main()            // the main function is the entry point of a C program, it
 
     // Who needs pepole when u are a programmer I TRUST the Machine and other programmers !♥.
 
+    //=========================================================================================================================================================================================
+    // logical operators in C:
+
+    /*logical operators are used to combine multiple conditions in a program
+
+     Used to "Combine or Modify boolean expressions" the conditions in a program,
+      they are used in if statements, while loops, and other control structures to create more complex conditions.
+
+                 The three logical operators in C are:
+                         && (logical AND)
+                         || (logical OR)
+                         !  (logical NOT) */
+
+    int tempratuer = 30;
+    if (tempratuer > 25 && tempratuer < 35) {
+        printf("The weather is hot\n");
+    } else if (tempratuer >= 15 && tempratuer <= 25) {
+        printf("The weather is warm\n");
+    } else {
+        printf("The weather is cold\n");
+    }
+
+    // another example:
+    bool sunny = true;
+
+    if (!sunny) {  // if not sunny
+        printf("It is raining\n");
+    } else {
+        printf("It is sunny\n");
+    }
+
+    //=========================================================================================================================================================================================
+
+  
+
     return 0;
 }
